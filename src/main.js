3c73428b29c42c09f74f8c6493778ec98e4a7cc1
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-03-02 20:10:52 +0000

    formato cafe

--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
 // Actividad 1
 
-console.log(`Actividad 1`)
+console.log(`Actividad 1`);
+console.log(`Vendemos 5 cafecitos con diferentes precios`);
+console.log(`Calcular con JS el más barato y el más caro`);
 
-const todaslasnotas = [10, 3, 4, 7, 1]
+const preciosDeCafe = [10, 3, 4, 7, 8]
 
-let min = todaslasnotas[0];
-for (let i = 0; i < todaslasnotas.length; i++) {
-    if (todaslasnotas[i] < min) {
-        min = todaslasnotas[i];
+let min = preciosDeCafe[0];
+for (let i = 0; i < preciosDeCafe.length; i++) {
+    if (preciosDeCafe[i] < min) {
+        min = preciosDeCafe[i];
     }
 }
 
-console.log(`La nota más baja es ${min}`);
+console.log(`El café más barato sale ${min}`);
 
-let max = todaslasnotas[0];
-for (let i = 0; i > todaslasnotas.length; i++) {
-    if (todaslasnotas[i] > max) {
-        min = todaslasnotas[i];
+let max = preciosDeCafe[0];
+for (let i = 0; i > preciosDeCafe.length; i++) {
+    if (preciosDeCafe[i] > max) {
+        min = preciosDeCafe[i];
     }
 }
 
-console.log(`La nota más alta es ${max}`);
+console.log(`El café más caro sale ${max}`);
 
 // Actividad 2
+
+console.log(`Actividad 2`)
+console.log(`El prompt inicial se utiliza para facilitar la entrega del vuelto al cliente`);
 
 let cafe = 3;
 let pago = prompt('El cliente acaba de comprar un café de $3. Ingresa con cuánto abona:');
