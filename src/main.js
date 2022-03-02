160bbc603c44bcf70b7f6256bd7695dca114c004
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-03-02 19:44:05 +0000

    actividad 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// Actividad 1
+
+console.log(`Actividad 1`)
+
 const todaslasnotas = [10, 3, 4, 7, 1]
 
 let min = todaslasnotas[0];
@@ -5,7 +9,7 @@ for (let i = 0; i < todaslasnotas.length; i++) {
     if (todaslasnotas[i] < min) {
         min = todaslasnotas[i];
     }
-} 
+}
 
 console.log(`La nota más baja es ${min}`);
 
@@ -14,6 +18,19 @@ for (let i = 0; i > todaslasnotas.length; i++) {
     if (todaslasnotas[i] > max) {
         min = todaslasnotas[i];
     }
-} 
+}
 
 console.log(`La nota más alta es ${max}`);
+
+// Actividad 2
+
+let cafe = 3;
+let pago = prompt('El cliente acaba de comprar un café de $3. Ingresa con cuánto abona:');
+pago = parseInt(pago);
+let vuelto = (pago - cafe);
+
+if (pago >= cafe) {
+    alert(`El vuelto es ${vuelto}`);
+}
+
+
