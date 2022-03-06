ee5d3a827480cc7c107b223143e85c37afe03a65
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-03-06 14:02:05 +0000

    desafio 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-// Actividad 1
+// Actividad 1: calculando precio menor y mayor
 
 console.log(`Actividad 1`);
 console.log(`Vendemos 5 cafecitos con diferentes precios`);
 console.log(`Calcular con JS el más barato y el más caro`);
 
-const preciosDeCafe = [10, 3, 4, 7, 8]
+const preciosDeCafe = [3, 3.5, 4]
 
 let min = preciosDeCafe[0];
 for (let i = 0; i < preciosDeCafe.length; i++) {
@@ -24,7 +24,7 @@ for (let i = 0; i > preciosDeCafe.length; i++) {
 
 console.log(`El café más caro sale ${max}`);
 
-// Actividad 2
+// Actividad 2: Vuelto 
 
 console.log(`Actividad 2`)
 console.log(`El prompt inicial se utiliza para facilitar la entrega del vuelto al cliente`);
@@ -37,5 +37,40 @@ let vuelto = (pago - cafe);
 if (pago >= cafe) {
     alert(`El vuelto es ${vuelto}`);
 }
+
+// Desafio 1: Simulador interactivo
+
+// productos es un objeto que mapea codigo de barras a productos
+// un producto tiene precio y nombre
+const productos = {
+    '4567864342': {'nombre': 'Americano', 'precio': 3},
+    '0987863433': {'nombre': 'Capuchino', 'precio': 3.5},
+    '2388909055': {'nombre': 'Moka', 'precio': 4},
+};
+
+// Dado un codigo de barras, devuelve el producto (nombre y precio)
+function buscarProducto(codigoDeBarras) {
+    return productos[codigoDeBarras];
+}
+
+function cobrarProducto(codigoDeBarras, dineroPagado) {
+    producto = buscarProducto(codigoDeBarras);
+
+    if (producto === undefined) {
+        console.log('Producto no encontrado');
+        return;
+    }
+
+    if (producto.precio <= dineroPagado) {
+        const vuelto = dineroPagado - producto.precio; 
+        console.log(`Cliente compro ${producto.nombre} y le tenemos que dar $${vuelto} de vuelto`);
+    } else {
+        const faltante = producto.precio - dineroPagado;
+        console.log(`Pedirle al cliente $${faltante} extra para poder venderle un ${producto.nombre}`);
+    }
+}
+
+cobrarProducto('0987863433', 4);
+
 
 
