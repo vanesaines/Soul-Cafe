bd956dee3883a04e5fb38ba5c6d369a568b7115e
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-03-14 23:15:52 +0000

    arrays2

--- src/main.js
+++ src/main.js
@@ -46,16 +46,17 @@ if (pago >= cafe) {
 // Desafio 1: Simulador interactivo
 // ############################################################
 
-document.write(`    <h1>HOLA MUNDO!</h1>
-<p>Queremos ayudar donando todas las ventas.</p>
-<p>Podés comprar cafecito desde $3.</p>
-<p>Que tengas hermoso día!!</p>
+document.write(`<h1>HOLA MUNDO! HOLA CAFETEROS!</h1>
+<p>La Fundación Todos Los Soles, está juntando fondos para ayudar a los niños desamparados por la inundación. <br> Nostros también queremos ayudar y lo haremos donando el 90% de nuestras ventas.</p>
+<p>Vos podés ser parte de esta ayuda con sólo comprar un cafecito.</p>
+<p>Gracias por ser parte!! Que tengas hermoso día!!</p>
 <br> <p>PRECIOS <hr> </p> 
 <p>Americano $3</p> 
 <p>Latte $3.50</p> 
-<p>Caramel +$0.5</p><br> `)
+<p>Caramel +$0.5</p>
+<p>Capuchino / Moka $4</p> <br>`)
 
-// productos es un objeto que mapea codigo de barras a productos
+// productos es un OBJETO que mapea codigo de barras a productos
 // un producto tiene nombre y precio
 const productos = {
     '4567864342': {'nombre': 'Americano', 'precio': 3},
@@ -103,12 +104,15 @@ if (pago >= cafe) {
 
 } */
 
-// CLASE ARRAYS: carrito
+// ************* CLASE ARRAYS *****************************
 
-document.write ('PRODUCTOS: <hr><br>');
+
+// PRODUCTOS CLASICOS *****************************
+
+document.write ('PRODUCTOS CLASICOS: <hr><br>');
 
 const productosSoloCafe = [
-    '- Café Americano LF: agua caliente y doble shot de espresso, con unas gotitas de leche fría',
+    'Café Americano LF: agua caliente y doble shot de espresso, con unas gotitas de leche fría',
     'Café Americano Regular: agua caliente y doble shot de espresso',
     'Café Americano LC: agua caliente y doble shot de espresso, con unas gotitas de leche caliente',
     'Café Single Espresso: un solo shot de espresso',
@@ -120,20 +124,64 @@ const productosCafeLeche = [
     'Café Latte Caramel: doble shot de espresso y leche, con caramelo.'
 ]
 
+// usando CONCAT (une arrays)
 const todosLosProductos = productosSoloCafe.concat(productosCafeLeche);
 
-document.write(todosLosProductos.join(';<br> - '));
+// usando JOIN (lo que va entre elementos)
+document.write(todosLosProductos.join(';<br>'));
+
+// PRODUCTOS NUEVOS *****************************
+
+document.write ('<br> <br>PRODUCTOS NUEVOS: <hr><br>');
+
+const productosNuevos = [
+    'Café Capuchino: doble shot de espresso, leche y chocolate arriba',
+    'Café Moka: chocolate fundido, doble shot de espresso, leche y chocolate arriba'
+];
+
+// usando SPREAD OPERATOR (une arrays)
+const todosTodosLosProductos = [ ...todosLosProductos, ...productosNuevos];
+
+// usando JOIN (lo que va entre elementos)
+document.write (productosNuevos.join(';<br>'));
+
+// PRODUCTOS CON LECHE *****************************
+
+// usando SLICE (copia)
+document.write ('<br><br>PRODUCTOS CON LECHE: <hr><br>');
+const todosCafesLeche = todosTodosLosProductos.slice(5,9)
+document.write (todosCafesLeche.join(';<br>'))
+
+// usando INDEXOF (indica posición)
+console.log(todosTodosLosProductos.indexOf('Café Latte Regular: doble shot de espresso y leche'));
+
+// usando INCLUDES (indica si existe)
+console.log(todosTodosLosProductos.includes('Café Latte Regular: doble shot de espresso y leche'));
+console.log(todosTodosLosProductos.includes('Café Latte'));
+
+// usando REVERSE (muestra al revés los elementos)
+console.log(todosTodosLosProductos.reverse());
+
+// CARRITO *****************************
 
 document.write ('<br><br> CARRITO DE COMPRAS: <hr><br>');
 
 const carrito = [];
 
-productosSoloCafe.pop();
+// usando SHIFT (elimina primer elemento)
 productosSoloCafe.shift();
+
+// usando POP (elimina último elemento)
+productosSoloCafe.pop();
+
+// usando SPLICE (borra de-a posición)
 productosSoloCafe.splice(1,1);
 
+// usando JOIN (lo que va entre elementos)
 carrito.push (productosSoloCafe.join('<br> + '));
-document.write (carrito);
+document.write (`${carrito} <br><br><br>`);
+
+
 
 
 
