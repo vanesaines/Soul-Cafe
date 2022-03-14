63c166fae5224318dfffc014db82e567b955f70c
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-03-14 21:48:23 +0000

    arrays

--- src/main.js
+++ src/main.js
@@ -46,15 +46,21 @@ if (pago >= cafe) {
 // Desafio 1: Simulador interactivo
 // ############################################################
 
-console.log(`Desafio 1: Simulador interactivo`)
-console.log(`Ejemplo al final del codigo`)
+document.write(`    <h1>HOLA MUNDO!</h1>
+<p>Queremos ayudar donando todas las ventas.</p>
+<p>Podés comprar cafecito desde $3.</p>
+<p>Que tengas hermoso día!!</p>
+<br> <p>PRECIOS <hr> </p> 
+<p>Americano $3</p> 
+<p>Latte $3.50</p> 
+<p>Caramel +$0.5</p><br> `)
 
 // productos es un objeto que mapea codigo de barras a productos
 // un producto tiene nombre y precio
 const productos = {
     '4567864342': {'nombre': 'Americano', 'precio': 3},
-    '0987863433': {'nombre': 'Capuchino', 'precio': 3.5},
-    '2388909055': {'nombre': 'Moka', 'precio': 4},
+    '0987863433': {'nombre': 'Latte', 'precio': 3.5},
+    '2388909055': {'nombre': 'Capuchino', 'precio': 4},
 };
 
 // Dado un codigo de barras, devuelve el producto (nombre y precio)
@@ -96,6 +102,38 @@ if (pago >= cafe) {
 }
 
 } */
+
+// CLASE ARRAYS: carrito
+
+document.write ('PRODUCTOS: <hr><br>');
+
+const productosSoloCafe = [
+    '- Café Americano LF: agua caliente y doble shot de espresso, con unas gotitas de leche fría',
+    'Café Americano Regular: agua caliente y doble shot de espresso',
+    'Café Americano LC: agua caliente y doble shot de espresso, con unas gotitas de leche caliente',
+    'Café Single Espresso: un solo shot de espresso',
+    'Café Doble Espresso: dos shots de espresso'
+    ];
+
+const productosCafeLeche = [
+    'Café Latte Regular: doble shot de espresso y leche', 
+    'Café Latte Caramel: doble shot de espresso y leche, con caramelo.'
+]
+
+const todosLosProductos = productosSoloCafe.concat(productosCafeLeche);
+
+document.write(todosLosProductos.join(';<br> - '));
+
+document.write ('<br><br> CARRITO DE COMPRAS: <hr><br>');
+
+const carrito = [];
+
+productosSoloCafe.pop();
+productosSoloCafe.shift();
+productosSoloCafe.splice(1,1);
+
+carrito.push (productosSoloCafe.join('<br> + '));
+document.write (carrito);
 
 
 
