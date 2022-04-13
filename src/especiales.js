e91401d81a2e780f8585220816994b64ec240bff
Vanesa Ochoa <ochoa.vanesaines@gmail.com>
2022-04-13 21:46:14 +0100

    fetch3

--- src/especiales.js
+++ src/especiales.js
@@ -1,10 +1,38 @@
 
+const ESPECIALES_API_URL = 'https://api.sampleapis.com/coffee/hot';
 
-const baseURL = 'https://api.sampleapis.com/coffee/hot';
-fetch(baseURL)
-  .then(resp => resp.json())
-  .then(data => displayData(data));
+function productoHTMLCard({title, description, ingredients}) {  
+  const ingredientes = ingredients.map((i) => `<a href="#" class="card-link">${i}</a>`).join('\n');
 
-function displayData(data) {
-  document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+  return `<div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${description}</h6>
+              <p class="card-text">Ingredientes:</p>
+                ${ingredientes}
+            </div>
+          </div>
+          <br>`;
+}
+
+function dibujarEspeciales(cafes) {
+  // 1 - Buscar un nodo en el DOM pre
+  const nodeEspeciales = document.querySelector('.especials');
+  // 2 - Inicializamos `nodeEspecialesContenido` igual al texto vacio
+  let nodeEspecialesContenido = '';
+  // 3 - Por cada cafe hacemos algo
+  for (let cafe of cafes){
+      nodeEspecialesContenido = nodeEspecialesContenido + productoHTMLCard(cafe);
+  }
+  // 4 - Al terminar el for, nodeEspecialesContenido tiene como valor todas las card de los cafes
+  // 5 - Le cambiamos el contenido al nodo .tienda para que tenga las cards
+  nodeEspeciales.innerHTML = nodeEspecialesContenido;
+}
+
+// Se ejecuta despues que el navegador termino de cargar todo
+window.onload = function(e) {
+  fetch(ESPECIALES_API_URL)
+    .then(resp => resp.json())
+    .then(data => data.filter((d) => d.id >= 5 && d.id <= 20))
+    .then(data => dibujarEspeciales(data));
 }
